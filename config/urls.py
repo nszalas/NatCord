df676a0d2dc485eca8d0a8c80a5a86ef208ae198
nszalas <66625813+Bonir2302@users.noreply.github.com>
2026-05-30 21:46:02 +0200

    Serve uploaded media on deployment

--- config/urls.py
+++ config/urls.py
@@ -17,16 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
     path('', include('chat.urls')),
     path('', include('moderation.urls')),
+    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
