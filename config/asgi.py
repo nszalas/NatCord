59da294c63c7747c9dbaad02692eea215f71a46e
nszalas <66625813+Bonir2302@users.noreply.github.com>
2026-05-30 21:10:35 +0200

    Fix ASGI initialization for deployment

--- config/asgi.py
+++ config/asgi.py
@@ -14,10 +14,13 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
 import chat.routing
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(URLRouter(
         chat.routing.websocket_urlpatterns
     ))
